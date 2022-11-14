56d54a26efa96396494d363bf7737ba20521e1e0
rufustyle <105512861+rufustyle@users.noreply.github.com>
2022-11-14 06:53:37 +0000

    Add files via upload

---
+++ rufus-hide-Twitch-redpill/index.js
@@ -0,0 +1,9 @@
+console.log("Porkixonas is enable")
+const divHide = document.getElementsByClassName("Layout-sc-nxg1ff-0 zJFAR side-nav side-nav--collapsed")[0]
+console.log(divHide)
+divHide.setAttribute("style","display:none;")
+
+// const divHide = document.getElementsByClassName("ScTransitionBase-sc-eg1bd7-0 dpqRKW tw-transition")
+// console.log(divHide)
+
+
